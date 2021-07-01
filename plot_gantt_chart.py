f9b468f7952835fb7c7178b91ea14ea284c3ed5b
YangGuangcan <guangcanyang@yeah.net>
2021-07-01 09:30:12 +0800

    Add files via upload.

--- plot_gantt_chart.py
+++ plot_gantt_chart.py
@@ -15,4 +15,4 @@ a = GanttChart("%s/%s" % (file_dir, file))  # 调用甘特图生成类
 a.gantt_chart_png(filename=file_save, fig_width=9, fig_height=5, random_colors=False, lang=0, dpi=200,
                   height=0.8, scale_more=12, x_step=a.schedule.makespan // 10, text_rotation=0,
                   with_operation=True, with_start_end=False, jobs_label=True, show=False)  # 绘制png格式的甘特图
-a.gantt_chart_html(filename=file_save, date="2021 06 26", lang=0, show=False)  # 绘制html格式的甘特图
+# a.gantt_chart_html(filename=file_save, date="2021 06 26", lang=0, show=False)  # 绘制html格式的甘特图
